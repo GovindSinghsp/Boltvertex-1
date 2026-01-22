ae4cc53e0178621fa71b98a4c05fd8ce1583d06f
GovindSinghsp <160107220+GovindSinghsp@users.noreply.github.com>
2026-01-22 19:14:33 +0530

    Fix import errors in Contact and Pricing pages

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import PricingHero from '../components/pricing/PricingHero';
-import PricingPlans from '../components/pricing/PricingPlans';
-import PricingComparison from '../components/pricing/PricingComparison';
-import CustomSolutions from '../components/pricing/CustomSolutions';
-import PricingFAQ from '../components/pricing/PricingFAQ';
+import Pricing from '../components/Pricing';
 
 const Pricing = () => {
   return (
     <div className="min-h-screen pt-16">
-      <PricingHero />
-      <PricingPlans />
-      <PricingComparison />
-      <CustomSolutions />
-      <PricingFAQ />
+      <Pricing />
     </div>
   );
 };
